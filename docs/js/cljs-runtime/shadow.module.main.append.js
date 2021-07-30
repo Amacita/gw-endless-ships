1d99ba8eb619e925c25d5f1ec21bf508ef5d655c
amacita <amacita8@gmail.com>
2021-07-30 14:38:56 -0400

    Removed debug version of cljs runtime

--- docs/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { endless_ships.core.init(); } catch (e) { console.error("An error occurred when calling (endless-ships.core/init)"); throw(e); }
